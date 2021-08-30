1936ce187b4f83f79a69acb80846db8fee23f16d
C1317873 Joao Pedro Barbosa de Oliveira <c1317873@interno.bb.com.br>
2021-08-30 13:30:29 -0300

    publicando app server expo

--- App.tsx
+++ App.tsx
@@ -1,15 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-
+import {StatusBar} from 'expo-status-bar';
+import React, {useEffect} from 'react';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+import * as Updates from 'expo-updates';
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
-
-export default function App() {
+const App: React.FC = () => {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
-
+  useEffect(() => {
+    async function updateApp() {
+      const {isAvailable} = await Updates.checkForUpdateAsync();
+      if (isAvailable) {
+        await Updates.fetchUpdateAsync();
+        await Updates.reloadAsync();
+      }
+    }
+    updateApp();
+  }, []);
   if (!isLoadingComplete) {
     return null;
   } else {
@@ -20,4 +28,5 @@ export default function App() {
       </SafeAreaProvider>
     );
   }
-}
+};
+export default App;
