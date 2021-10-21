7f1913498b493a529624c77fa01ef1fd2c069a70
Gabriel Lins <linsgb2@gmail.com>
2021-10-21 02:03:58 -0300

    posts

--- services/post.ts
+++ services/post.ts
@@ -1,32 +1,37 @@
-import axios from 'axios'
+import axios from 'axios';
 
-const api = axios.create({baseURL: 'http://192.168.0.22:8000'})
+const api = axios.create({baseURL: 'http://192.168.0.22:8000'});
 
 const listPosts = async () => {
-    return (await api.get('/projects')).data
-}
+  return (await api.get('/projects')).data;
+};
 
 const createComment = async (payload: TComment) => {
-    return await api.post('/coments', payload)
-}
+  return await api.post('/coments', payload);
+};
 
 const createPost = async (payload: TPost) => {
-    return await api.post('/projects', payload)
-}
+  const bodyFormData = new FormData();
+  console.log("TEST => ", payload)
+  bodyFormData.append('project_name', payload.project_name);
+  bodyFormData.append('file', payload.file);
+  return await api.post('/projects', bodyFormData, {
+    headers: {
+        accept: 'application/json',
+        "Content-Type": 'multipart/form-data'
+    },
+  });
+};
 
 type TComment = {
-    project: number,
-    comment: string
-}
+  project: number;
+  comment: string;
+};
 
 type TPost = {
-    project_name: string,
-    file: string,
-    user: number
-}
+  project_name: string;
+  file: any;
+  user: number;
+};
 
-export {
-    createPost,
-    createComment,
-    listPosts
-}
+export {createPost, createComment, listPosts};
