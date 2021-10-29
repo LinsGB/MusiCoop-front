811c6db50bf70a6ec8f212e1b591a5edc504b430
C1317873 Joao Pedro Barbosa de Oliveira <c1317873@interno.bb.com.br>
2021-10-29 18:13:30 -0300

    ajuste post

--- screens/ModalScreenPost.tsx
+++ screens/ModalScreenPost.tsx
@@ -1,5 +1,5 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useState, useEffect } from 'react';
+import {StatusBar} from 'expo-status-bar';
+import React, {useState, useEffect} from 'react';
 import {
   Platform,
   StyleSheet,
@@ -10,22 +10,22 @@ import {
   Button,
 } from 'react-native';
 import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
+import {Text, View} from '../components/Themed';
 import Colors from '../constants/Colors';
 import logoUsuario from '../assets/images/fffa.png';
 import * as DocumentPicker from 'expo-document-picker';
 import clipe from '../assets/images/clipe.png';
 import reactotron from 'reactotron-react-native';
 import AudioPlayer from '../components/audioPlayer';
-import { createContribuition } from '../services/post'
-import { getContribution, getMusic } from '../services/music'
+import {createContribuition} from '../services/post';
+import {getContribution, getMusic} from '../services/music';
 
-const ModalScreenPost = ({ route }: { route: any }) => {
-  const [comment, setComment] = useState('')
-  const [contribuitions, setContribuitions] = useState([])
-  const [uri, setUri] = useState('')
-  const [fileName, setFileName] = useState('')
-  const [textValue, setTextValue] = useState('')
+const ModalScreenPost = ({route}: {route: any}) => {
+  const [comment, setComment] = useState('');
+  const [contribuitions, setContribuitions] = useState([]);
+  const [uri, setUri] = useState('');
+  const [fileName, setFileName] = useState('');
+  const [textValue, setTextValue] = useState('');
 
   useEffect(() => {
     setContribuitions(route.params.item.contribuitions);
@@ -46,10 +46,10 @@ const ModalScreenPost = ({ route }: { route: any }) => {
   };
 
   const postContribuition = async () => {
-    setTextValue('')
-    const copyContribuitions = [...contribuitions]
-    copyContribuitions.push({name: comment, uri})
-    setContribuitions(copyContribuitions)
+    setTextValue('');
+    const copyContribuitions = [...contribuitions];
+    copyContribuitions.push({name: comment, uri});
+    setContribuitions(copyContribuitions);
     const type = 'audio/mpeg';
     const file = {
       uri: uri,
@@ -57,17 +57,19 @@ const ModalScreenPost = ({ route }: { route: any }) => {
       type,
     };
     //@ts-ignore
-    console.log(await createContribuition(items.id, {
-      name: comment,
-      description: comment,
-      file
-    }))
-  }
+    console.log(
+      await createContribuition(items.id, {
+        name: comment,
+        description: comment,
+        file,
+      }),
+    );
+  };
 
   return (
     <React.Fragment>
       <View style={styles.container}>
-        <ScrollView style={{ backgroundColor: 'white' }}>
+        <ScrollView style={{backgroundColor: 'white'}}>
           <View
             style={{
               flexDirection: 'row',
@@ -88,30 +90,33 @@ const ModalScreenPost = ({ route }: { route: any }) => {
           </View>
           <View>
             <Text style={styles.title}>{items.post_name}</Text>
-            <AudioPlayer
-              uri={`https://musicoop-api.herokuapp.com/musics?post_id=${items.id}`}
-            />
             <View style={styles.getStartedContainer}>
               <Text
                 style={styles.getStartedText}
                 lightColor="rgba(0,0,0,0.8)"
                 darkColor="rgba(255,255,255,0.8)">
-                Conteudo do post
+                {items.description}
               </Text>
               {contribuitions &&
                 contribuitions.map((contribuition: any) => (
                   <View>
                     <Text>{contribuition.name}</Text>
                     <AudioPlayer
-                      uri={contribuition.uri || `https://musicoop-api.herokuapp.com/musics?contribuition_id=${contribuition.id}`}
+                      uri={
+                        contribuition.uri ||
+                        `https://musicoop-api.herokuapp.com/musics?contribuition_id=${contribuition.id}`
+                      }
                     />
                   </View>
                 ))}
             </View>
+            <AudioPlayer
+              uri={`https://musicoop-api.herokuapp.com/musics?post_id=${items.id}`}
+            />
           </View>
         </ScrollView>
       </View>
-      <View style={{ borderTopWidth: 1, borderColor: '#eee' }}>
+      <View style={{borderTopWidth: 1, borderColor: '#eee'}}>
         <View
           style={{
             flexDirection: 'row',
@@ -129,8 +134,12 @@ const ModalScreenPost = ({ route }: { route: any }) => {
               }}
             />
           </TouchableOpacity>
-          <View style={{ marginTop: 10 }}>
-            <Text onPress={() => postContribuition()} style={{ fontWeight: 'bold' }}>Enviar</Text>
+          <View style={{marginTop: 10}}>
+            <Text
+              onPress={() => postContribuition()}
+              style={{fontWeight: 'bold'}}>
+              Enviar
+            </Text>
           </View>
         </View>
         <TextInput
@@ -145,8 +154,8 @@ const ModalScreenPost = ({ route }: { route: any }) => {
           }}
           value={textValue}
           onChangeText={(text) => {
-            setComment(text)
-            setTextValue(text)
+            setComment(text);
+            setTextValue(text);
           }}
           underlineColorAndroid="transparent"
           placeholder="Faça uma colaboração"></TextInput>
