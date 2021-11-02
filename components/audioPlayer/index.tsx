1b2f6e97c0c95f43c05b8ff52fed961c02c2e1c1
C1317873 Joao Pedro Barbosa de Oliveira <c1317873@interno.bb.com.br>
2021-11-02 17:35:20 -0300

    adicionado loading no front

--- components/audioPlayer/index.tsx
+++ components/audioPlayer/index.tsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {ActivityIndicator, StyleSheet, Text, View} from 'react-native';
 import {Ionicons} from '@expo/vector-icons';
 import {Audio} from 'expo-av';
 import reactotron from 'reactotron-react-native';
@@ -7,7 +7,7 @@ const AudioPlayer = (props: any) => {
   const [isPlaying, setIsPlaying] = useState(false);
   const [playbackObject, setPlaybackObject] = useState<any>(null);
   const [playbackStatus, setPlaybackStatus] = useState<any>(null);
-  const [song, setSong] = useState();
+  const [loading, setLoading] = useState(false);
   useEffect(() => {
     if (playbackObject === null) {
       setPlaybackObject(new Audio.Sound());
@@ -24,6 +24,7 @@ const AudioPlayer = (props: any) => {
   const handleAudioPlayPause = async () => {
     if (playbackObject !== null && playbackStatus === null) {
       console.log('1');
+      setLoading(true);
       const status = await playbackObject.loadAsync({uri}, {shouldPlay: true});
       setIsPlaying(true);
       playbackObject.setOnPlaybackStatusUpdate(async (status: any) => {
@@ -33,45 +34,40 @@ const AudioPlayer = (props: any) => {
           await setIsPlaying(false);
           setPlaybackObject(new Audio.Sound());
         }
+        if (status.isPlaying === true) {
+          setLoading(false);
+        }
       });
       return setPlaybackStatus(status);
     }
     // It will pause our audio
     if (isPlaying) {
-      console.log('2');
       const status = await playbackObject.pauseAsync();
       setIsPlaying(false);
       return setPlaybackStatus(status);
     }
     // It will resume our audio
     if (!isPlaying) {
-      console.log('3');
+      setLoading(true);
       let status;
       try {
         status = await playbackObject.playAsync();
       } catch (error) {
         status = await playbackObject.loadAsync({uri}, {shouldPlay: true});
       }
+      setIsPlaying(true);
       playbackObject.setOnPlaybackStatusUpdate(async (status: any) => {
         if (status.didJustFinish === true) {
-          console.log('CABOU');
           await playbackObject.unloadAsync();
-          reactotron.debug('acabou');
           await setIsPlaying(false);
           setPlaybackObject(new Audio.Sound());
         }
+        if (status.isPlaying === true) {
+          setLoading(false);
+        }
       });
-      setIsPlaying(true);
       return setPlaybackStatus(status);
     }
-    if (playbackStatus.didJustFinish) {
-      reactotron.debug('ACABOOOOO');
-    }
-  };
-  const restartSong = async () => {
-    const status = await playbackObject.replayAsync();
-    setIsPlaying(false);
-    return setPlaybackStatus(status);
   };
   return (
     <View
@@ -83,18 +79,27 @@ const AudioPlayer = (props: any) => {
         maxWidth: 45,
       }}>
       {/* <Text style={{fontSize: 18, marginBottom: 15}}>{audio.filename}</Text> */}
-      <Ionicons
-        style={{
-          backgroundColor: 'gray',
-          padding: 10,
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}
-        name={isPlaying ? 'pause' : 'play'}
-        size={24}
-        color="white"
-        onPress={handleAudioPlayPause}
-      />
+      <View style={{flexDirection: 'row'}}>
+        <Ionicons
+          style={{
+            backgroundColor: 'gray',
+            padding: 10,
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+          name={isPlaying ? 'pause' : 'play'}
+          size={24}
+          color="white"
+          onPress={handleAudioPlayPause}
+        />
+        {loading && (
+          <ActivityIndicator
+            style={{marginLeft: 5}}
+            size="large"
+            color="#c8c8c8"
+          />
+        )}
+      </View>
     </View>
   );
 };
